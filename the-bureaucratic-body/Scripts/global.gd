bda716d07d1919c8d592fed3ee98f7eb2eef24ee
Jennifer Tyler <83067625+chickenjenders@users.noreply.github.com>
2026-05-04 00:07:01 -0400

    Remove boring music integration and related functionality from global script

--- the-bureaucratic-body/Scripts/global.gd
+++ the-bureaucratic-body/Scripts/global.gd
@@ -1,20 +1,6 @@
 extends Node
 
-const BORING_MUSIC: AudioStream = preload("res://assets/boring.ogg")
-
 var professionalism = 50
 var anxiety = 50
 var agency = 10
 var current_outfit = "none" # Tracks the "Morning Ritual" choice [cite: 29]
-
-var _music_player: AudioStreamPlayer
-
-func _ready() -> void:
-	_music_player = AudioStreamPlayer.new()
-	_music_player.stream = BORING_MUSIC
-	_music_player.finished.connect(_on_music_finished)
-	add_child(_music_player)
-	_music_player.play()
-
-func _on_music_finished() -> void:
-	_music_player.play()
